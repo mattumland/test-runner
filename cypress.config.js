d5b2f698608de2be3ccfff46ab7e29aa6727c8e2
Matt Umland <mattumland@gmail.com>
2023-10-09 15:27:46 -0400

    WIP - manual login test

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://storefront-elb-254850467.us-east-1.elb.amazonaws.com/',
+    baseUrl: 'http://ec2-18-215-170-129.compute-1.amazonaws.com/',
+    pageLoadTimeout: 180000,
+    retries: {
+      runMode: 3,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,21 @@
 describe('E2E spec', () => {
   it('visits the staging site', () => {
-    cy.visit('/')
+    cy.visit('/Login')
+    cy.get('a[href*="AuthRedirect?"]').click()
+    cy.origin('https://testulplatform.b2clogin.com', () => {
+      cy.get('#signInName').type(Cypress.env('account_email'))
+      cy.get('#continue').click()
+      cy.get('#password').type(Cypress.env('login'))
+      cy.get('#continue').click()
+    })
+    cy.get('a[href*="AuthRedirect?"]').should('be.visible')
+    cy.reload()
+
+    cy.get('h1').should('be visible')
+    // .should(($h1) => {
+    //   expect($h1).to.contain('migrationtest@myyahoo.com')
+    // })
+
+
   })
 })
